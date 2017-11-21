b433daa9c25d6c27e7b5d65783b2c482c2693dbc
LiaoSheng <liaosheng779@gmail.com>
2017-11-21 14:14:39 +0800

    delete

---
+++ src/main/java/com/example/Filter/MyFilter.java
@@ -0,0 +1,29 @@
+package com.example.Filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * Created by liaosheng on 2017/8/25.
+ */
+public class MyFilter implements Filter {
+    @Override
+    public void destroy() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void doFilter(ServletRequest srequest, ServletResponse sresponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        // TODO Auto-generated method stub
+        HttpServletRequest request = (HttpServletRequest) srequest;
+        System.out.println("this is MyFilter,url :"+request.getRequestURI());
+        filterChain.doFilter(srequest, sresponse);
+    }
+
+    @Override
+    public void init(FilterConfig arg0) throws ServletException {
+        System.out.println("init");
+    }
+}
